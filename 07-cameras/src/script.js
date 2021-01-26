ece5894daf0dfbf76290c23b776c1a9cf057ba15
Girish V <girish.v@needl.ai>
2021-01-26 10:52:29 +0530

    Orbit Controls

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 /**
  * Base
@@ -7,13 +8,17 @@ import * as THREE from "three";
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
+const cursor = { x: 0, y: 0 };
+canvas.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = event.clientY / sizes.height - 0.5;
+});
+
 // Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-
-const aspectRatio = sizes.width / sizes.height;
 
 // Scene
 const scene = new THREE.Scene();
@@ -26,20 +31,23 @@ const mesh = new THREE.Mesh(
 scene.add(mesh);
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
-const camera = new THREE.OrthographicCamera(
-  -1 * aspectRatio,
-  1 * aspectRatio,
-  1,
-  -1,
-  0.1,
-  100
-);
-camera.position.x = 2;
-camera.position.y = 2;
-camera.position.z = 2;
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
+// const camera = new THREE.OrthographicCamera(
+//   -1 * aspectRatio,
+//   1 * aspectRatio,
+//   1,
+//   -1,
+//   0.1,
+//   100
+// );
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 3;
 camera.lookAt(mesh.position);
 scene.add(camera);
+
+const control = new OrbitControls(camera, canvas);
+control.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -48,13 +56,19 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 
 // Animate
-const clock = new THREE.Clock();
+// const clock = new THREE.Clock();
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+  // const elapsedTime = clock.getElapsedTime();
 
   // Update objects
-  mesh.rotation.y = elapsedTime;
+  // mesh.rotation.y = elapsedTime;
+
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 3;
+  // camera.lookAt(mesh.position);
+  control.update();
 
   // Render
   renderer.render(scene, camera);
